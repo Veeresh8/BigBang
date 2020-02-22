a4408ca61c864f7a2258e1e964daf5647b46a48c
Veeresh <veeresh.charantimath8@gmail.com>
2020-02-22 22:43:54 +0530

    add(App.kt, Modules.kt, AndroidManifest.xml): using Koin to decouple dependencies. Koin being a service-locator is a better choice in my opinion for small-medium projects.

---
+++ app/src/main/java/com/droid/bigbang/Modules.kt
@@ -0,0 +1,20 @@
+@file:Suppress("USELESS_CAST")
+
+package com.droid.bigbang
+
+import com.droid.bigbang.data.DataRepository
+import com.droid.bigbang.data.DataRepositoryImpl
+import com.google.gson.Gson
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+// GLOBAL DEPENDENCIES
+val applicationModule = module {
+    single { Gson() }
+    factory<DataRepository> { DataRepositoryImpl() }
+}
+
+// VIEW-MODEL
+val photoViewModel = module {
+    viewModel { PhotoViewModel(dataRepository = get()) }
+}
