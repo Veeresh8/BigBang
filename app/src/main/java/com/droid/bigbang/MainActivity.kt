ca5de50749c4788d3f6578ba63dfcbdfade0be2f
Veeresh <veeresh.charantimath8@gmail.com>
2020-02-23 20:48:23 +0530

    update(MainActivity.kt, Extensions.kt, activity_main.xml): handling successful photo parse and error state

--- app/src/main/java/com/droid/bigbang/MainActivity.kt
+++ app/src/main/java/com/droid/bigbang/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import com.droid.bigbang.models.PhotoParseResult
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.retry_layout.*
 import org.koin.android.ext.android.inject
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     private val photoViewController: PhotoViewController by inject()
     private val photoViewModel: PhotoViewModel by inject()
-
     private val SPAN_COUNT = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +24,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun fetchPhotos() {
-        photoViewModel.fetchPhotos(R.raw.data).observe(this, Observer {  })
+        photoViewModel.fetchPhotos(R.raw.data).observe(this, Observer {
+            when (it) {
+                is PhotoParseResult.PhotoParseException -> {
+                    handleError(it)
+                }
+                is PhotoParseResult.PhotosList -> {
+                    showPhotos(it)
+                }
+            }
+        })
+    }
+
+    private fun showPhotos(parseResult: PhotoParseResult.PhotosList) {
+        photoViewController.photoList = parseResult.photoList.toMutableList()
+    }
+
+    private fun handleError(exception: PhotoParseResult.PhotoParseException) {
+        Timber.e("Exception parsing resources ${exception.message}")
+        photoView.gone()
+        errorLayout.visible()
+        tvErrorMessage.text = resources.getString(R.string.error_message)
     }
 
     private fun initRecyclerView(span: Int) {
         val layoutManager = GridLayoutManager(this, span)
         photoViewController.spanCount = span
         layoutManager.spanSizeLookup = photoViewController.spanSizeLookup
+        photoView.layoutManager = layoutManager
         photoView.setController(photoViewController)
     }
 }
