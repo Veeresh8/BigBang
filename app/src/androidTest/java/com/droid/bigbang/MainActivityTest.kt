bb2bc27f000a8bcec5b7fd8a25e4a317fafb8b12
Veeresh <veeresh.charantimath8@gmail.com>
2020-02-24 03:49:52 +0530

    test: add passing test to fetch photo data

--- app/src/androidTest/java/com/droid/bigbang/MainActivityTest.kt
+++ app/src/androidTest/java/com/droid/bigbang/MainActivityTest.kt
@@ -3,21 +3,27 @@ package com.droid.bigbang
 import androidx.lifecycle.MutableLiveData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.internal.inject.InstrumentationContext
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import com.droid.bigbang.data.DataRepository
+import com.droid.bigbang.models.Photo
 import com.droid.bigbang.models.PhotoParseResult
+import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import org.hamcrest.CoreMatchers.not
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.KoinTest
@@ -69,5 +75,30 @@ class MainActivityTest : KoinTest {
 
         onView(withId(R.id.photoView))
             .check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun shouldFetchDataSuccessfully() {
+        val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val photosJSON = targetContext.resources.openRawResource(R.raw.data).bufferedReader()
+            .use { it.readText() }
+        val photoList = Gson().fromJson(photosJSON, Array<Photo>::class.java).toList()
+
+        val liveData = MutableLiveData<PhotoParseResult>()
+        liveData.postValue(PhotoParseResult.PhotosList(photoList))
+
+        whenever(mockDataRepository.getPhotos(any())).thenReturn(liveData)
+
+        activity.launchActivity(null)
+
+        onView(withId(R.id.photoView))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.errorLayout))
+            .check(matches(not(isDisplayed())))
+
+        onView(withId(R.id.photoView)).check(RecyclerViewItemCountAssertion(photoList.size))
+
     }
 }
