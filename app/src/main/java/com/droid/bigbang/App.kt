acd5bdf81d8c645071523eec3d8ee996434338a4
Veeresh <veeresh.charantimath8@gmail.com>
2020-02-24 00:35:24 +0530

    update(App.kt): making Glide hold tag objects and timber logger init

--- app/src/main/java/com/droid/bigbang/App.kt
+++ app/src/main/java/com/droid/bigbang/App.kt
@@ -1,9 +1,11 @@
 package com.droid.bigbang
 
 import android.app.Application
+import com.bumptech.glide.request.target.ViewTarget
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class App: Application() {
 
@@ -13,6 +15,10 @@ class App: Application() {
             androidLogger()
             androidContext(this@App)
             modules(listOf(applicationModule, photoViewModel))
+        }
+        ViewTarget.setTagId(R.id.glideTag)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
