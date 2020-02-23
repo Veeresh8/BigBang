3a7ba219914116c97b9c9d05c2fdd74fb7ae86c0
Veeresh <veeresh.charantimath8@gmail.com>
2020-02-24 01:02:03 +0530

    add(PhotoDetailViewController.kt): handling horizontal scroll for photo selected

---
+++ app/src/main/java/com/droid/bigbang/PhotoDetailViewController.kt
@@ -0,0 +1,35 @@
+@file:Suppress("UNUSED_ANONYMOUS_PARAMETER")
+
+package com.droid.bigbang
+
+import com.airbnb.epoxy.AsyncEpoxyController
+import com.airbnb.epoxy.carousel
+import com.droid.bigbang.models.Photo
+
+class PhotoDetailViewController : AsyncEpoxyController() {
+
+    var scrollPosition = 0
+
+    var photoList: List<Photo> = emptyList()
+        set(value) {
+            field = value
+            requestModelBuild()
+        }
+
+    override fun buildModels() {
+         val photoDetail = photoList.map {
+             PhotoDetailView_()
+                 .id(it.url)
+                 .photo(it)
+         }
+
+        carousel {
+            id("photo_carousel")
+            models(photoDetail)
+            numViewsToShowOnScreen(1.2F)
+            onBind { model, view, position ->
+                view.scrollToPosition(scrollPosition)
+            }
+        }
+    }
+}
