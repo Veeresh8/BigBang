b62a86733c09d8bc506d2daecc2382c14197c351
Veeresh <veeresh.charantimath8@gmail.com>
2020-02-23 20:40:51 +0530

    update(DataRepositoryImpl.kt): update live-data with necessary states

--- app/src/main/java/com/droid/bigbang/data/DataRepositoryImpl.kt
+++ app/src/main/java/com/droid/bigbang/data/DataRepositoryImpl.kt
@@ -1,10 +1,22 @@
 package com.droid.bigbang.data
 
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import com.droid.bigbang.R
+import com.droid.bigbang.models.Photo
 import com.droid.bigbang.models.PhotoParseResult
+import com.google.gson.Gson
 
-class DataRepositoryImpl: DataRepository {
+class DataRepositoryImpl(private val context: Context, private val gson: Gson): DataRepository {
     override fun getPhotos(path: Int, photoLiveData: MutableLiveData<PhotoParseResult>): MutableLiveData<PhotoParseResult> {
+        try {
+            val photosJSON = context.resources.openRawResource(R.raw.data).bufferedReader().use { it.readText() }
+            val photoList = gson.fromJson(photosJSON, Array<Photo>::class.java).toList()
+            photoLiveData.postValue(PhotoParseResult.PhotosList(photoList))
+        } catch (exception: Exception) {
+            photoLiveData.postValue(PhotoParseResult.PhotoParseException(exception.message.toString()))
+        }
+
         return photoLiveData
     }
 }
