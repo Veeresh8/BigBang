db55997a9a24fed3115171ddb6c5849fbf567874
Veeresh <veeresh.charantimath8@gmail.com>
2020-02-24 00:34:41 +0530

    update(Photo.kt): implements parcelable

--- app/src/main/java/com/droid/bigbang/models/Photo.kt
+++ app/src/main/java/com/droid/bigbang/models/Photo.kt
@@ -5,14 +5,14 @@ import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Photo (val copyright: String,
-                  val date: String,
-                  val explanation: String,
-                  @SerializedName("hdurl") val originalURL: String,
-                  @SerializedName("media_type") val mediaType: String,
-                  @SerializedName("service_version") val serviceVersion: String,
-                  val title: String,
-                  val url: String): Parcelable
+data class Photo (val copyright: String?,
+                  val date: String?,
+                  val explanation: String?,
+                  @SerializedName("hdurl") val originalURL: String?,
+                  @SerializedName("media_type") val mediaType: String?,
+                  @SerializedName("service_version") val serviceVersion: String?,
+                  val title: String?,
+                  val url: String?): Parcelable
 
 sealed class PhotoParseResult {
     class PhotosList(val photoList: List<Photo>) : PhotoParseResult()
