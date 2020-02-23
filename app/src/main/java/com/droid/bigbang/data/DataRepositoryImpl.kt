b5593fce91da3a4cf54e027be86d3aab055b8b5c
Veeresh <veeresh.charantimath8@gmail.com>
2020-02-23 19:43:13 +0530

    update(DataRepositoryImpl.kt): change data source path from String -> Int

--- app/src/main/java/com/droid/bigbang/data/DataRepositoryImpl.kt
+++ app/src/main/java/com/droid/bigbang/data/DataRepositoryImpl.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import com.droid.bigbang.models.PhotoParseResult
 
 class DataRepositoryImpl: DataRepository {
-    override fun getPhotos(path: String, photoLiveData: MutableLiveData<PhotoParseResult>): MutableLiveData<PhotoParseResult> {
+    override fun getPhotos(path: Int, photoLiveData: MutableLiveData<PhotoParseResult>): MutableLiveData<PhotoParseResult> {
         return photoLiveData
     }
 }
